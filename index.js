a6fbafd2776b9245b177b8b64a902effd253933b
Denis <denis.were@student.moringaschool.com>
2023-03-18 22:52:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+
 function receivesAFunction(callback) {
-    if (typeof callback === 'function') {
-      callback();
-    } else {
-      throw new Error('Argument must be a function');
-    }
+  callback(); 
+}
+
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("This is a named function");
   }
-  function returnsANamedFunction() {
-    function namedFunction() {
-      console.log('Hello, world!');
-    }
-    return namedFunction;
-  }
-  function returnsAnAnonymousFunction() {
-    return function() {
-      console.log('Hello, world!');
-    }
-  }
-  
+  return namedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("This is an anonymous function");
+  };
+}
